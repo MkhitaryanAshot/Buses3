b57ab8977a0b82860664f9c3d03fbf1cf92e0efc
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2024-04-10 11:20:40 +0400

    FINISHED THE WORK

--- Buses3/Buses3.cpp
+++ Buses3/Buses3.cpp
@@ -3,30 +3,151 @@
 
 using namespace std;
 
-int main() {
-    Graph graph(5);
+void testcase1(Graph& graph) {
+    graph.newGraph(11);
 
-    graph.addEdge("B1", "B2", 9);
-    graph.addEdge("B1", "B3", 4);
+
+    graph.addEdge("B1", "B9", 5);
+    graph.addEdge("B2", "B3", 2);
     graph.addEdge("B3", "B4", 3);
-    graph.addEdge("B4", "B2", 1);
+    graph.addEdge("B3", "B4", 3);
+    graph.addEdge("B3", "B6", 4);
+    graph.addEdge("B6", "B5", 2);
+    graph.addEdge("B5", "B1", 3);
+    graph.addEdge("B4", "B7", 8);
+    graph.addEdge("B6", "B8", 3);
+    graph.addEdge("B7", "B8", 4);
+    graph.addEdge("B9", "B8", 6);
+    graph.addEdge("B10", "B9", 7);
+    graph.addEdge("B8", "B11", 6);
+    graph.addEdge("B11", "B10", 5);
+};
+
+
+void testcase2(Graph& graph) {
+    graph.newGraph(6);
+
+    graph.addEdge("B1", "B2", 4);
+    graph.addEdge("B2", "B3", 9);
+    graph.addEdge("B1", "B4", 2);
     graph.addEdge("B4", "B5", 6);
-    graph.addEdge("B2", "B5", 5);
-
-    cout << endl << "--------------- our graph -------------------" << endl;
-
-    graph.printMatrix(graph.matrix);
-
-    cout << endl << "------- after using floyd warshall ----------" << endl;
+    graph.addEdge("B3", "B6", 5);
+    graph.addEdge("B6", "B5", 3);
+    graph.addEdge("B5", "B1", 1);
+    graph.addEdge("B5", "B3", 4);
 
 
+}
 
-    vector<string> buses;
-    buses.push_back("B5");
-    buses.push_back("B4");
-    buses.push_back("B3");
-    buses.push_back("B2");
+void testcase3(Graph& graph) {
+    graph.newGraph(8);
 
 
-    graph.add_bus(buses.size(), "B1", buses);
+    graph.addEdge("B1", "B2", 2);
+    graph.addEdge("B1", "B3", 1);
+    graph.addEdge("B2", "B3", 3);
+    graph.addEdge("B2", "B4", 4);
+    graph.addEdge("B3", "B4", 1);
+    graph.addEdge("B3", "B5", 7);
+    graph.addEdge("B4", "B6", 5);
+    graph.addEdge("B5", "B6", 2);
+    graph.addEdge("B6", "B7", 3);
+    graph.addEdge("B7", "B8", 4);
+    graph.addEdge("B8", "B5", 2);
+
+}
+
+int main() {
+    cout << "Choose the options" << endl;
+    cout << "1. Test case 1" << endl;
+    cout << "2. Test case 2" << endl;
+    cout << "3. Test case 3" << endl;
+    cout << "4. Manual input" << endl;
+
+    int x;
+    cin >> x;
+
+    Graph graph(0);
+
+    if (x == 1) {
+        testcase1(graph);
+    }
+    else if (x == 2) {
+        testcase2(graph);
+    }
+    else if (x == 3) {
+        testcase3(graph);
+    }
+    else if (x == 4) {
+        int n;
+        int m;
+
+        cout << "Enter the number of crossroads ";
+        cin >> n;
+
+        cout << "Enter the number of streets ";
+        cin >> m;
+
+        graph.newGraph(n);
+
+        while (m != 0) {
+            string u;
+            string v;
+            int k;
+
+            cout << "Enter first street ";
+            cin >> u;
+
+            cout << "Enter second street ";
+            cin >> v;
+
+            cout << "Enter the duration in minutes required to drive through the street ";
+            cin >> k;
+
+            graph.addEdge(u, v, k);
+
+            m--;
+        }
+        cout << "------------Your graph------------" << endl;
+        graph.printMatrix(graph.matrix);
+
+
+    }
+    else {
+        cout << "Invalid option" << endl;
+        return 0;
+    }
+
+    vector<vector<int>> reserved = graph.floydWarshall();
+    cout << endl;
+
+    while (true) {
+        cout << "Choose the exercise to start" << endl;
+        cout << "1. Add Bus" << endl;
+        cout << "2. Construct Crossroad" << endl;
+        cout << "3. Common Streets" << endl;
+        cout << "4. Exit" << endl;
+        int k;
+        cin >> k;
+
+        if (k == 1) {
+            graph.add_bus(reserved);
+        }
+        else if (k == 2) {
+            graph.Construct_crossroad();
+        }
+        else if (k == 3) {
+            graph.Common_Streets();
+        }
+        else if (k == 4) {
+            break;
+        }
+        else {
+            cout << "Invalid option" << endl;
+        }
+        cout << endl;
+    }
+
+
+    return 0;
 }
--- Buses3/Exercise2.cpp
+++
@@ -1,1 +0,0 @@
-#include "Exercise2.h"
